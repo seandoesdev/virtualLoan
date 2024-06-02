9f5e61e61d214bd48d3caad31e16696aca07e5bf
seandoesdev <seandoesdev@gmail.com>
2024-06-02 20:14:21 +0900

    Initial commit

---
+++ src/main/java/com/showLoan/virtualLoan/VirtualLoanApplication.java
@@ -0,0 +1,13 @@
+package com.showLoan.virtualLoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showLoan/virtualLoan/VirtualLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showLoan.virtualLoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
